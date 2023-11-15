e5b245ede6ed9e767e933fe40cc743a86074feef
johnietre <johnietrebus@gmail.com>
2023-11-15 16:50:53 -0600

    Error update

--- rust/src/error.rs
+++ rust/src/error.rs
@@ -1,15 +1,14 @@
 use crate::{Headers, Message, Response, WriteUnpin, BV};
+use anyhow::Error as AnyError;
+use std::borrow::Cow;
 use std::error::Error as StdError;
 use std::fmt;
 use std::io::Error as IOError;
-use tokio::io::AsyncWrite;
 
 pub type Res<T> = Result<T, JtRPCError>;
 pub type ArcRes<T> = Result<T, std::sync::Arc<JtRPCError>>;
 
 pub type DynError = Box<dyn StdError>;
-
-pub type DynResponse = Response<Box<dyn AsyncWrite>>;
 
 // Used for flags
 #[allow(dead_code)]
@@ -20,6 +19,30 @@ pub struct ErrResp {
     pub path: String,
     pub headers: Headers,
     pub body: BV,
+}
+
+impl ErrResp {
+    pub fn take_body(&mut self) -> BV {
+        std::mem::replace(&mut self.body, Vec::new())
+    }
+
+    pub fn body_str(&self) -> Result<&str, std::str::Utf8Error> {
+        std::str::from_utf8(&self.body)
+    }
+
+    pub fn take_body_string(&mut self) -> Result<String, std::string::FromUtf8Error> {
+        let body = self.take_body();
+        String::from_utf8(body)
+    }
+
+    pub fn body_string_lossy<'a>(&'a self) -> Cow<'a, str> {
+        String::from_utf8_lossy(&self.body)
+    }
+
+    pub fn take_body_string_lossy(&mut self) -> String {
+        let body = self.take_body();
+        String::from_utf8_lossy(&body).into_owned()
+    }
 }
 
 impl<W: WriteUnpin> From<Response<W>> for ErrResp {
@@ -34,6 +57,21 @@ impl<W: WriteUnpin> From<Response<W>> for ErrResp {
     }
 }
 
+impl fmt::Display for ErrResp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // TODO: Body?
+        write!(
+            f,
+            "Status Code: {}, Path: \"{}\", Body: {}",
+            self.status_code,
+            self.path,
+            self.body_string_lossy()
+        )
+    }
+}
+
+impl StdError for ErrResp {}
+
 #[derive(Debug)]
 pub enum JtRPCError {
     // TODO
@@ -42,7 +80,7 @@ pub enum JtRPCError {
     ClientClosed,
     StreamClosed(Option<Message>),
     TimedOut,
-    Other(DynError),
+    Other(AnyError),
 }
 
 impl From<ErrResp> for JtRPCError {
@@ -63,8 +101,8 @@ impl From<IOError> for JtRPCError {
     }
 }
 
-impl From<DynError> for JtRPCError {
-    fn from(err: DynError) -> Self {
+impl From<AnyError> for JtRPCError {
+    fn from(err: AnyError) -> Self {
         JtRPCError::Other(err)
     }
 }
@@ -75,7 +113,7 @@ impl fmt::Display for JtRPCError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         use JtRPCError::*;
         match self {
-            Response(r) => write!(f, "Received non-200 response: {r:?}"),
+            Response(r) => write!(f, "Received non-200 response: {r}"),
             IO(e) => write!(f, "{e}"),
             ClientClosed => write!(f, "client is closed"),
             StreamClosed(None) => write!(f, "stream is closed"),
